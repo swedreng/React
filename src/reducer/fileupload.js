af264fd17497624a0cb917614a81eac17442efb6
swedreng <anil-swedreng@hotmail.com>
2018-01-25 12:19:42 +0300

    updated.

--- src/reducer/fileupload.js
+++ src/reducer/fileupload.js
@@ -1,7 +1,6 @@
 import { FILE_UPLOAD , PP_UPLOAD } from "../constants"
 const defaultState = {
-  result:null,
-  pp_result:null
+  result:null
 }
 
 export default (state = defaultState, action = {}) => {
@@ -10,15 +9,6 @@ export default (state = defaultState, action = {}) => {
       return {
         ...state,
         result:action.payload
-      }
-      break
-      case PP_UPLOAD:
-      console.log(action.payload,9)
-      const image = (action.payload !== null ? `${process.env.URL}${action.payload}` : null)
-      console.log(image,7)
-      return {
-        ...state,
-        pp_result:image
       }
       break
     default:
