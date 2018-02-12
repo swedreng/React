a3f9c7140609e1201bfea5e8ecbbcc92f9bf90cd
swedreng <anil-swedreng@hotmail.com>
2018-02-12 17:52:26 +0300

    Updated.

--- src/components/profile/share/write.js
+++ src/components/profile/share/write.js
@@ -1,11 +1,15 @@
 
 import React, {Component} from 'react';
+import './write.scss'
 
 class write extends Component{
     render(){
         return(
             <div className="row">
-               write
+                <div className="form-group write">
+                    <textarea className="form-control" rows="5" id="comment" placeholder="Bir şeylerden bahset .."></textarea>
+                    <button className="pull-right" type="button" className="btn btn-success">Gönderiyi paylaş</button>
+                </div>
             </div>
         );
     }
